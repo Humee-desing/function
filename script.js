acc482fc57833ae046ab2493e863afbc01f93a27
Humee-desing <humaira020602@gmail.com>
2025-06-25 10:59:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let score = 100-0
+
+if ( (score >= 80) &&  (score <=100)) {
+    console.log("A")
+} else{ (score >=80) && (score <= 100)
+    console.log("A")
+}
+
+if ( (score >= 79) &&  (score <=60)) {
+    console.log("B")
+} else{ (score >=79) && (score <= 60)
+    console.log("B")
+}
+
+if ( (score >= 59) &&  (score <=40)) {
+    console.log("C")
+} else{ (score >=59) && (score <= 40)
+    console.log("C")
+}
+
+if ( (score >= 39) &&  (score <=40)) {
+    console.log("D")
+} else{ (score >=39) && (score <= 40)
+    console.log("D")
+}
+
+if ( (score >= 19) &&  (score <=0)) {
+    console.log("E")
+} else{ (score >=19) && (score <= 0)
+    console.log("E")
+}
+
